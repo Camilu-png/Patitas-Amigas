a1cad178dc11dc5b220625a3b8df27891f67df01
Camilú <camilu@fabian-VivoBook-ASUSLaptop-X421FAY-X413FA@gmail.com>
2022-07-14 00:10:18 -0400

    feat(main.dart): home Scaffold appBar and body

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,16 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Center(
+          child: Text("Walcome, Camilú"),
+        ),
+      )//const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }

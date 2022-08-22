960a91251001aec5a43b44b1a9900ac48377aa50
Camilu-png <camilachillan99@gmail.com>
2022-08-22 01:18:25 -0400

    feat(gradient_back.dart): create GradientBack

---
+++ lib/gradient_back.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/main.dart';
+
+class GradientBack extends StatelessWidget{
+  String title = "Adoptados";
+
+  GradientBack(this.title);
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      height: 250.0,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [
+              Palette.kToDark.shade300,
+              Palette.kToDark.shade100
+            ],
+          begin: FractionalOffset(0.5, 0.0),
+          end: FractionalOffset(1.0, 1.6),
+          stops: [0.0, 0.6],
+            tileMode: TileMode.clamp
+        )
+      ),
+      child: Text(
+          title,
+          style: TextStyle(
+              color: Colors.white,
+            fontSize: 25.0,
+            fontFamily: "Poppins",
+            fontWeight: FontWeight.bold
+      ),
+      ),
+      alignment: Alignment(-0.9, -0.6),
+
+    );
+  }
+}

a7748b9732f0218e00c87dc6f871acc8ff06b287
Camilu-png <camilachillan99@gmail.com>
2022-08-22 01:18:54 -0400

    feat(header_appbar.dart): create HeaderAppBar

---
+++ lib/header_appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/card_image_list.dart';
+import 'package:platzi_trips_app/gradient_back.dart';
+
+class HeaderAppBar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Stack(
+      children: <Widget>[
+        GradientBack("Adoptados"),
+        CardImageList()
+      ],
+    );
+  }
+}

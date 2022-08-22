732f4d14ca24782aaeadb639f6640d7500dffefb
Camilu-png <camilachillan99@gmail.com>
2022-08-21 21:52:50 -0400

    feat(main.dart): create new palette

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 import 'description_place.dart';
 
+class Palette {
+  static const MaterialColor kToDark = const MaterialColor(
+    0xff77d6ce,
+    const <int, Color>{
+      50: const Color(0xff6bc1b9),//10%
+      100: const Color(0xff5faba5),//20%
+      200: const Color(0xff539690),//30%
+      300: const Color(0xff47807c),//40%
+      400: const Color(0xff3c6b67),//50%
+      500: const Color(0xff305652),//60%
+      600: const Color(0xff24403e),//70%
+      700: const Color(0xff182b29),//80%
+      800: const Color(0xff0c1515),//90%
+      900: const Color(0xff000000),//100%
+    },
+  );
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -23,12 +41,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Palette.kToDark,
       ),
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.pets),
-          title: Text('Home'),
+          leading: Icon(
+            Icons.pets,
+          color: Palette.kToDark.shade700,
+          ),
+          title: Text('Adoptados'),
         ),
         body: new DescriptionPlace(),
       )//const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,15 +59,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,11 +71,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -78,28 +85,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(

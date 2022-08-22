42a9b30221f6c9394e37fc5ad78a07a754e1a61a
Camilu-png <camilachillan99@gmail.com>
2022-08-21 23:24:10 -0400

    feat(main.dart: add godparent.dart and ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'description_place.dart';
+import 'godparent.dart';
 
 class Palette {
   static const MaterialColor kToDark = const MaterialColor(
@@ -32,18 +33,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Palette.kToDark,
       ),
       home: Scaffold(
+          resizeToAvoidBottomInset: false,
         appBar: AppBar(
           leading: Icon(
             Icons.pets,
@@ -51,11 +44,20 @@ class MyApp extends StatelessWidget {
           ),
           title: Text('Adoptados'),
         ),
-        body: new DescriptionPlace(),
-      )//const MyHomePage(title: 'Flutter Demo Home Page'),
+        body: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(20.0),
+          children: <Widget>[
+            new DescriptionPlace(),
+            new GodParent(),
+    ]
+        ))
     );
   }
 }
+//new DescriptionPlace(),
+//               new GodParent(),
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -77,13 +79,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(widget.title),
       ),

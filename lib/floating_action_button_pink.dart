a08ded500c134c874a59afca96dcc7429240de9e
Camilu-png <camilachillan99@gmail.com>
2022-08-22 21:10:58 -0400

    feat(floating_action_button_pink.dart): create class FloatingActionButtonPink and _FloatingActionButtonPink

---
+++ lib/floating_action_button_pink.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonPink extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FloatingActionButtonPink();
+  }
+}
+
+class _FloatingActionButtonPink extends State<FloatingActionButtonPink>{
+  bool _pressed = false;
+
+  void onPressedFav(){
+    setState(() {
+      _pressed = !this._pressed;
+    });
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: this._pressed ? Text("Adoptar") : Text("Cancelar adopción"),
+          )
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: Color(0xFFD6777F),
+      tooltip: "Adoptar",
+      onPressed: onPressedFav,
+      child: Icon(
+          this._pressed ? Icons.favorite : Icons.favorite_border
+      ),
+    );
+  }
+}

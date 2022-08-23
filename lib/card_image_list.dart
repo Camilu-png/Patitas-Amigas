0e3bdbdc5b95221991cbd0fb020f51afd973df88
Camilu-png <camilachillan99@gmail.com>
2022-08-22 21:12:37 -0400

    feat(card_image_list.dart): add FloatingActionButtonPink() in a Stack

--- lib/card_image_list.dart
+++ lib/card_image_list.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'card_image.dart';
+import 'floating_action_button_pink.dart';
 
 class CardImageList extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Container(
       height: 350.0,
-      child: ListView(
-        padding: EdgeInsets.all(25.0),
-        scrollDirection: Axis.horizontal,
+      child: Stack(
+        alignment: Alignment(0.69,1.0),
         children: <Widget>[
-          CardImage("android/assets/img/pose.jpg"),
-          CardImage("android/assets/img/Vaquita.jpg"),
-          CardImage("android/assets/img/VaquitaSeguimiento.jpg"),
+          ListView(
+            padding: EdgeInsets.all(25.0),
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+
+              CardImage("android/assets/img/pose.jpg"),
+              CardImage("android/assets/img/Vaquita.jpg"),
+              CardImage("android/assets/img/VaquitaSeguimiento.jpg"),
+            ],
+          ),
+          FloatingActionButtonPink(),
         ],
       ),
+
+
     );
   }
 }

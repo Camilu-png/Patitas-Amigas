1639bc88930ae24b5965e7bf5e6c3040bbc89e21
Camilu-png <camilachillan99@gmail.com>
2022-08-26 13:25:03 -0400

    refactor(home_adoption.dart): update ListView to Stack

--- lib/home_adoption.dart
+++ lib/home_adoption.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'godson.dart';
+import 'adoption_list.dart';
+import 'gradient_back.dart';
 
 class HomeAdoption extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Stack(
         children: <Widget>[
-          GodSon("android/assets/img/pose.jpg", "Vaquita", "Viña del Mar", "esterilizado", "hembra"),
-          GodSon("android/assets/img/luther.jpg", "Luther", "Valparaíso", "esterilizado", "macho"),
-          GodSon("android/assets/img/rayo.jpg", "Rayo", "Villa Alemana", "esterilizado", "macho"),
-          GodSon("android/assets/img/pose.jpg", "Vaquita", "2", "esterilizado", "macho"),
-          GodSon("android/assets/img/pose.jpg", "Vaquita", "2", "esterilizado", "macho"),
+          GradientBack("Adopción"),
+          AdoptionList()
         ]
     );
   }

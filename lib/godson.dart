2331ee03af70cdae2f31837f85a2be8f1feb6a28
Camilu-png <camilachillan99@gmail.com>
2022-08-26 12:33:12 -0400

    feat(godson.dart): create godson.dart

---
+++ lib/godson.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/main.dart';
+
+class GodSon extends StatelessWidget{
+
+  String pathImage;
+  String name;
+  String city;
+  String extra;
+  String gender;
+
+  GodSon(this.pathImage, this.name, this.city, this.extra, this.gender);
+  @override
+  Widget build(BuildContext context){
+    final picture = Container(
+      height: 250.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(10.0),
+            topLeft: Radius.circular(10.0)),
+        image: DecorationImage(
+          image: AssetImage(pathImage),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+
+    final female  = Container(
+      margin: const EdgeInsets.only(
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.female,
+        color: Color(0xFFD6777F),
+      ),
+    );
+    final male  = Container(
+      margin: const EdgeInsets.only(
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.male,
+        color: Palette.kToDark.shade100,
+      ),
+    );
+    
+    final description = Container(
+      height: 50.0,
+      color: Colors.white,
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Text(
+              name,
+            style: TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 16.0,
+            ),
+          ),
+          Spacer(),
+          Text(
+              city,
+            style: TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 15.0,
+            ),
+          ),
+          gender == "macho" ? male : female,
+        ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Color(0xFA6A6A6FF),
+              blurRadius: 10.0,
+              offset: Offset(0.0,4.0)
+          )
+        ],
+      ),
+      padding: const EdgeInsets.all(5.0),
+      margin: const EdgeInsets.only(
+          top: 20.0,
+          left: 20.0,
+          right: 20.0
+      ),
+
+      child: Column(
+        children: <Widget>[
+          picture,
+          description
+        ],
+      ),
+    );
+  }
+}

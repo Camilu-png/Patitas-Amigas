39d8023109ad79fcfe313a5ba2269fe7faa37983
Camilu-png <camilachillan99@gmail.com>
2022-08-30 22:40:58 -0400

    fix(pet_profile.dart): add Material()

--- lib/pet_profile.dart
+++ lib/pet_profile.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-
 import 'description_place.dart';
 import 'godparent.dart';
 import 'header_appbar.dart';
 
 class PetProfile extends StatelessWidget{
+  String name;
+
+  PetProfile(this.name);
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Material(child:Stack(
       children: <Widget>[
         ListView(
             shrinkWrap: true,
             padding: const EdgeInsets.all(20.0),
             children: <Widget>[
-              DescriptionPlace(),
+              DescriptionPlace(name),
               GodParent("android/assets/img/perfil.jpg", "Camila Arancibia"),
             ]
         ),
         HeaderAppBar()
       ],
+    )
     );
   }
 }
